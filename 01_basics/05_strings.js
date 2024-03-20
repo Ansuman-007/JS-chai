82b4799617201f1d4559739c878ce9e173683802
ANSUMAN PADHI <139491282+Ansuman-007@users.noreply.github.com>
2024-03-20 08:25:07 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+const name = "ansh"
+const dogName= new String("Tomy-xzvv")
+const repoCount =50
+// console.log (name+ repoCount +"is best"); old methord donot use this
+console.log(`My name is ${name} ,i have repo Count in git hub is ${ repoCount}`);
+console.log(name[0]);
+console.log(dogName.toUpperCase());
+
+// console.log(dogName.length);
+// console.log(dogName.toUpperCase());
+console.log(dogName.charAt(2));
+console.log(dogName.indexOf('t'));
+
+
+const newString = dogName.substring
+console.log(newString);
+
+const anotherString = dogName.slice(-8, 4)
+console.log(anotherString);
+
+
+const newStringOne = "   ansuman    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url = 'ansumanpadhi.tech/ansuman%20/padhi'
+console.log(url.replace('%20','-'));
+console.log(url.includes('ansuman'));
+
+console.log(url.split('/'))
+
